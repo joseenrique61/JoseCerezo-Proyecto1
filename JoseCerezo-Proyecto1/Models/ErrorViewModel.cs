8a12e1e3fd5e1c86586f63582412e5e5d534cad0
laboratorio <laboratorio@UPOAULA10303>
2024-10-02 08:20:01 -0500

    Agregar archivos de proyecto.

---
+++ JoseCerezo-Proyecto1/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace JoseCerezo_Proyecto1.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
