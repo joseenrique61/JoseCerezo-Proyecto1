1153af248fad5d4f7ff831d81fef12ada5a70e12
laboratorio <laboratorio@UPOAULA10303>
2024-10-09 08:39:45 -0500

    Función de sumar

---
+++ JoseCerezo-Proyecto1/wwwroot/js/sumar.js
@@ -0,0 +1,6 @@
+﻿function sumar() {
+    var number1 = parseInt(document.getElementById("number1").value);
+    var number2 = parseInt(document.getElementById("number2").value);
+    
+    document.getElementById("resultado").value = number1 + number2;
+}
